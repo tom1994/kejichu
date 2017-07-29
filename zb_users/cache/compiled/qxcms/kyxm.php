03d68fc3b00aedc0ded4af119f5cee129b8b2d8f
yuanbaby <122931556@qq.com>
2017-07-29 20:31:34 +0800

    2017年7月29日20:31:19 科研项目首页模板

---
+++ zb_users/cache/compiled/qxcms/kyxm.php
@@ -0,0 +1,159 @@
+<?php  /* Template Name:科研项目首页 */  ?>
+<?php  include $this->GetTemplate('header');  ?>
+<!--面包屑导航-->
+<?php  include $this->GetTemplate('breadcrumb');  ?>
+<!-- 焦点文章开始 -->
+<div class="top-content">
+    <?php 
+    $acate = $zbp->Config('qxcms')->Acate;
+    $ason1 = $zbp->Config('qxcms')->Ason1;
+    $ason2 = $zbp->Config('qxcms')->Ason2;
+    $ason3 = $zbp->Config('qxcms')->Ason3;
+     ?>
+
+<!-- cms2开始 -->
+<?php if ($zbp->Config('qxcms')->Bcms=='1') { ?>
+<div class="cms-content">
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>时政/人事</h2>
+            <span><img src="<?php  echo $host;  ?>zb_users/theme/<?php  echo $theme;  ?>/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            <?php  $btuid=GetPost((int)$zbp->Config('qxcms')->Btuid);  ?>
+            <a href="<?php  echo $btuid->Url;  ?>"><img src="<?php  echo qxcms_FirstIMG($btuid,200,100);  ?>" alt="<?php  echo $btuid->Title;  ?>">
+                <h3><?php  echo $btuid->Title;  ?></h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                <?php  foreach ( GetList(4,$ason1) as $key=>$article) { ?>
+                <li><a href="<?php  echo $article->Url;  ?>"><?php  echo $article->Title;  ?></a></li>
+                <?php }   ?>
+            </ul>
+        </div>
+
+    </div>
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>国际</h2>
+            <span><img src="<?php  echo $host;  ?>zb_users/theme/<?php  echo $theme;  ?>/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            <?php  $btuid=GetPost((int)$zbp->Config('qxcms')->Btuid);  ?>
+            <a href="<?php  echo $btuid->Url;  ?>"><img src="<?php  echo qxcms_FirstIMG($btuid,200,100);  ?>" alt="<?php  echo $btuid->Title;  ?>">
+                <h3><?php  echo $btuid->Title;  ?></h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                <?php  foreach ( GetList(4,$ason1) as $key=>$article) { ?>
+                <li><a href="<?php  echo $article->Url;  ?>"><?php  echo $article->Title;  ?></a></li>
+                <?php }   ?>
+            </ul>
+        </div>
+    </div>
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>财经</h2>
+            <span><img src="<?php  echo $host;  ?>zb_users/theme/<?php  echo $theme;  ?>/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            <?php  $btuid=GetPost((int)$zbp->Config('qxcms')->Btuid);  ?>
+            <a href="<?php  echo $btuid->Url;  ?>"><img src="<?php  echo qxcms_FirstIMG($btuid,200,100);  ?>" alt="<?php  echo $btuid->Title;  ?>">
+                <h3><?php  echo $btuid->Title;  ?></h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                <?php  foreach ( GetList(4,$ason1) as $key=>$article) { ?>
+                <li><a href="<?php  echo $article->Url;  ?>"><?php  echo $article->Title;  ?></a></li>
+                <?php }   ?>
+            </ul>
+        </div>
+
+    </div>
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>法制/社会</h2>
+            <span><img src="<?php  echo $host;  ?>zb_users/theme/<?php  echo $theme;  ?>/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            <?php  $btuid=GetPost((int)$zbp->Config('qxcms')->Btuid);  ?>
+            <a href="<?php  echo $btuid->Url;  ?>"><img src="<?php  echo qxcms_FirstIMG($btuid,200,100);  ?>" alt="<?php  echo $btuid->Title;  ?>">
+                <h3><?php  echo $btuid->Title;  ?></h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                <?php  foreach ( GetList(4,$ason1) as $key=>$article) { ?>
+                <li><a href="<?php  echo $article->Url;  ?>"><?php  echo $article->Title;  ?></a></li>
+                <?php }   ?>
+            </ul>
+        </div>
+
+    </div>
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>证券</h2>
+            <span><img src="<?php  echo $host;  ?>zb_users/theme/<?php  echo $theme;  ?>/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            <?php  $btuid=GetPost((int)$zbp->Config('qxcms')->Btuid);  ?>
+            <a href="<?php  echo $btuid->Url;  ?>"><img src="<?php  echo qxcms_FirstIMG($btuid,200,100);  ?>" alt="<?php  echo $btuid->Title;  ?>">
+                <h3><?php  echo $btuid->Title;  ?></h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                <?php  foreach ( GetList(4,$ason1) as $key=>$article) { ?>
+                <li><a href="<?php  echo $article->Url;  ?>"><?php  echo $article->Title;  ?></a></li>
+                <?php }   ?>
+            </ul>
+        </div>
+    </div>
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>研究观点</h2>
+            <span><img src="<?php  echo $host;  ?>zb_users/theme/<?php  echo $theme;  ?>/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            <?php  $btuid=GetPost((int)$zbp->Config('qxcms')->Btuid);  ?>
+            <a href="<?php  echo $btuid->Url;  ?>"><img src="<?php  echo qxcms_FirstIMG($btuid,200,100);  ?>" alt="<?php  echo $btuid->Title;  ?>">
+                <h3><?php  echo $btuid->Title;  ?></h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                <?php  foreach ( GetList(4,$ason1) as $key=>$article) { ?>
+                <li><a href="<?php  echo $article->Url;  ?>"><?php  echo $article->Title;  ?></a></li>
+                <?php }   ?>
+            </ul>
+        </div>
+
+    </div>
+</div>
+<?php } ?>
+<!-- cms2结束 -->
+
+<!-- 友情链接开始 -->
+<div class="link">
+    <ul>
+        <li>友情链接:</li>
+        <?php  if(isset($modules['link'])){echo $modules['link']->Content;}  ?>
+    </ul>
+</div>
+<!-- 友情链接结束 -->
+<?php  include $this->GetTemplate('footer');  ?>
---
+++ zb_users/theme/qxcms/template/kyxm.php
@@ -0,0 +1,159 @@
+{* Template Name:科研项目首页 *}
+{template:header}
+<!--面包屑导航-->
+{template:breadcrumb}
+<!-- 焦点文章开始 -->
+<div class="top-content">
+    {php}
+    $acate = $zbp->Config('qxcms')->Acate;
+    $ason1 = $zbp->Config('qxcms')->Ason1;
+    $ason2 = $zbp->Config('qxcms')->Ason2;
+    $ason3 = $zbp->Config('qxcms')->Ason3;
+    {/php}
+
+<!-- cms2开始 -->
+{if $zbp->Config('qxcms')->Bcms=='1'}
+<div class="cms-content">
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>时政/人事</h2>
+            <span><img src="{$host}zb_users/theme/{$theme}/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            {$btuid=GetPost((int)$zbp->Config('qxcms')->Btuid)}
+            <a href="{$btuid.Url}"><img src="{qxcms_FirstIMG($btuid,200,100)}" alt="{$btuid.Title}">
+                <h3>{$btuid.Title}</h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                {foreach GetList(4,$ason1) as $key=>$article}
+                <li><a href="{$article.Url}">{$article.Title}</a></li>
+                {/foreach}
+            </ul>
+        </div>
+
+    </div>
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>国际</h2>
+            <span><img src="{$host}zb_users/theme/{$theme}/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            {$btuid=GetPost((int)$zbp->Config('qxcms')->Btuid)}
+            <a href="{$btuid.Url}"><img src="{qxcms_FirstIMG($btuid,200,100)}" alt="{$btuid.Title}">
+                <h3>{$btuid.Title}</h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                {foreach GetList(4,$ason1) as $key=>$article}
+                <li><a href="{$article.Url}">{$article.Title}</a></li>
+                {/foreach}
+            </ul>
+        </div>
+    </div>
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>财经</h2>
+            <span><img src="{$host}zb_users/theme/{$theme}/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            {$btuid=GetPost((int)$zbp->Config('qxcms')->Btuid)}
+            <a href="{$btuid.Url}"><img src="{qxcms_FirstIMG($btuid,200,100)}" alt="{$btuid.Title}">
+                <h3>{$btuid.Title}</h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                {foreach GetList(4,$ason1) as $key=>$article}
+                <li><a href="{$article.Url}">{$article.Title}</a></li>
+                {/foreach}
+            </ul>
+        </div>
+
+    </div>
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>法制/社会</h2>
+            <span><img src="{$host}zb_users/theme/{$theme}/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            {$btuid=GetPost((int)$zbp->Config('qxcms')->Btuid)}
+            <a href="{$btuid.Url}"><img src="{qxcms_FirstIMG($btuid,200,100)}" alt="{$btuid.Title}">
+                <h3>{$btuid.Title}</h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                {foreach GetList(4,$ason1) as $key=>$article}
+                <li><a href="{$article.Url}">{$article.Title}</a></li>
+                {/foreach}
+            </ul>
+        </div>
+
+    </div>
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>证券</h2>
+            <span><img src="{$host}zb_users/theme/{$theme}/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            {$btuid=GetPost((int)$zbp->Config('qxcms')->Btuid)}
+            <a href="{$btuid.Url}"><img src="{qxcms_FirstIMG($btuid,200,100)}" alt="{$btuid.Title}">
+                <h3>{$btuid.Title}</h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                {foreach GetList(4,$ason1) as $key=>$article}
+                <li><a href="{$article.Url}">{$article.Title}</a></li>
+                {/foreach}
+            </ul>
+        </div>
+    </div>
+    <div class="cms-kyxm">
+        <!--分割线-->
+        <div class="cms-kyxm-halving-line"></div>
+        <div class="cms-kyxm-title">
+            <h2>研究观点</h2>
+            <span><img src="{$host}zb_users/theme/{$theme}/image/focus.png"></span>
+        </div>
+        <div class="tu-kyxm-f">
+            {$btuid=GetPost((int)$zbp->Config('qxcms')->Btuid)}
+            <a href="{$btuid.Url}"><img src="{qxcms_FirstIMG($btuid,200,100)}" alt="{$btuid.Title}">
+                <h3>{$btuid.Title}</h3>
+            </a>
+        </div>
+        <div class="cms-kyxm-lr-list">
+            <ul>
+                {foreach GetList(4,$ason1) as $key=>$article}
+                <li><a href="{$article.Url}">{$article.Title}</a></li>
+                {/foreach}
+            </ul>
+        </div>
+
+    </div>
+</div>
+{/if}
+<!-- cms2结束 -->
+
+<!-- 友情链接开始 -->
+<div class="link">
+    <ul>
+        <li>友情链接:</li>
+        {module:link}
+    </ul>
+</div>
+<!-- 友情链接结束 -->
+{template:footer}
