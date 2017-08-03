8816e3ec9f210971100bbd007de9e4d550d74037
yuanbaby <122931556@qq.com>
2017-08-03 15:34:31 +0800

    2017年8月3日15:34:04 要闻首页页面样式和子类模板添加

--- zb_users/cache/compiled/qxcms/wbyj.php
+++ zb_users/cache/compiled/qxcms/wbyj.php
@@ -4,15 +4,7 @@
 <?php  include $this->GetTemplate('breadcrumb');  ?>
 <!-- 焦点文章开始 -->
 <div class="top-content">
-    <?php 
-    $acate = $zbp->Config('qxcms')->Acate;
-    $ason1 = $zbp->Config('qxcms')->Ason1;
-    $ason2 = $zbp->Config('qxcms')->Ason2;
-    $ason3 = $zbp->Config('qxcms')->Ason3;
-     ?>
 
-    <!-- cms2开始 -->
-    <?php if ($zbp->Config('qxcms')->Bcms=='1') { ?>
     <div class="cms-content">
         <div class="cms-wbyj-zxdj">
             <div class="cms-title-wbyj">
@@ -89,8 +81,6 @@
             </div>
         </div>
     </div>
-    <?php } ?>
-    <!-- cms2结束 -->
 
     <!-- 友情链接开始 -->
     <div class="link">
