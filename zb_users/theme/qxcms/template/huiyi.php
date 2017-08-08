9365f7eb56bd948b87c35c7326f5788c6c2b5b94
DESKTOP-OIRA2CN\Zhongruiguang <v-ruzhon@microsoft.com>
2017-08-09 00:30:02 +0800

    Fix Style

--- zb_users/theme/qxcms/template/huiyi.php
+++ zb_users/theme/qxcms/template/huiyi.php
@@ -7,8 +7,45 @@
     <span class="navbar-left-title">会议动态</span>
 </div>
 <!-- 左侧选项卡开始 -->
-<div class="navbar-left">
-
+<style  type="text/css">
+.time{
+    width: 100%;
+    height: 50px;
+    text-align: center;
+    color: white;
+    display: inline-block;
+    line-height: 50px;
+}
+.time :hover{
+background-color:#004170;
+}
+</style>
+<div class="" style="float:left
+">
+    <div style="
+    width: 80px;
+    background: #005999;
+    height: 475px;
+            "><span style="
+    color: white;
+    display: inline-block;
+    background-color: #2485CC;
+    border-radius: 4px;
+    width: 80%;
+    text-align: center;
+    /* margin-left: 10px; */
+    margin-left: 10%;
+    margin-top: 20px;
+    font-size: 14px;
+">时间优先</span>
+<span class="time">2017.06</span>
+<span class="time">2017.05</span>
+<span class="time">2017.04</span>
+<span class="time">2017.03</span>
+<span class="time">2017.02</span>
+<span class="time">2017.01</span>
+<span class="time">2016.12</span>
+<span class="time">2016.11</span></div>
 </div>
 <!-- 左侧选项卡结束 -->
 
