61dcd42ae955529b2d011afc0608bbffcc30ae16
DESKTOP-OIRA2CN\Zhongruiguang <v-ruzhon@microsoft.com>
2017-08-09 00:04:04 +0800

    Huiyi

---
+++ zb_users/theme/qxcms/template/huiyi.php
@@ -0,0 +1,21 @@
+{* Template Name:会议动态 *}
+{template:header}
+
+    <script type="text/javascript" src="{$host}zb_users/theme/{$theme}/js/jquery-3.2.1.js"></script>
+<div class="top-banner">
+    <img alt="" src="{$host}zb_users/theme/{$theme}/image/bn1.jpg">
+    <span class="navbar-left-title">会议动态</span>
+</div>
+<!-- 左侧选项卡开始 -->
+<div class="navbar-left">
+
+</div>
+<!-- 左侧选项卡结束 -->
+
+<!-- 右侧页面内容开始 -->
+<div class="main-content">
+
+</div>
+<!-- 右侧页面内容结束 -->
+
+{template:footer}
