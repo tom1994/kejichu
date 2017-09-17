07beecd44ec8a62eef816fa58a5f476f356729e7
yuanbaby <122931556@qq.com>
2017-09-17 20:53:23 +0800

    2017年9月17日20:42:47 搜索结果分类信息带上父类类名

--- zb_users/cache/compiled/qxcms/post-search.php
+++ zb_users/cache/compiled/qxcms/post-search.php
@@ -9,7 +9,7 @@ $cont = qxcms_jj($article,1,100,'...')
     <div class="list-msg">
         <span><?php  echo $article->Time('Y年m月d日 H:i:s');  ?></span>
         <span>点击:<?php  echo $article->ViewNums;  ?></span>
-        <span>分类:<?php  echo $article->Category->Name;  ?></span>
+        <span>分类:<?php  echo $article->Category->Parent->Name;  ?>:<?php  echo $article->Category->Name;  ?></span>
     </div>
     <p><?php  echo str_replace($q, $qc, $cont);  ?></p>   <!--str_replace居然在上面的php里不能用，放在这里居然能用，搞不懂，妈拉个鸡！！！-->
 </div>
