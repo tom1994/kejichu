969a24249cdb57f14040d1ff6da5e70c65f18147
yuanbaby <122931556@qq.com>
2017-09-18 00:46:46 +0800

    2017年9月18日00:46:30 智库中心搜索结果分页

--- zb_users/theme/qxcms/template/post-search.php
+++ zb_users/theme/qxcms/template/post-search.php
@@ -3,13 +3,15 @@ $q = $article->Q;
 $qc = '<b style=\'color:red\'>' . $q . '</b>';
 $cont = qxcms_jj($article,1,100,'...')
 {/php}
-<div class="horizon-line bg-lightgray"></div>
-<div class="search-list-item">
-    <h3 style="width: 100%"><a href="{$article.Url}">{$article.Title}</a></h3>
-    <div class="list-msg">
-        <span>{$article.Time('Y年m月d日 H:i:s')}</span>
-        <span>点击:{$article.ViewNums}</span>
-        <span>分类:{$article.Category.Parent.Name}:{$article.Category.Name}</span>
+<div class="zkzx_item">
+    <div class="horizon-line bg-lightgray"></div>
+    <div class="search-list-item">
+        <h3 style="width: 100%"><a href="{$article.Url}">{$article.Title}</a></h3>
+        <div class="list-msg">
+            <span>{$article.Time('Y年m月d日 H:i:s')}</span>
+            <span>点击:{$article.ViewNums}</span>
+            <span>分类:{$article.Category.Parent.Name}》{$article.Category.Name}</span>
+        </div>
+        <p>{str_replace($q, $qc, $cont)}</p>   <!--str_replace居然在上面的php里不能用，放在这里居然能用，搞不懂，妈拉个鸡！！！-->
     </div>
-    <p>{str_replace($q, $qc, $cont)}</p>   <!--str_replace居然在上面的php里不能用，放在这里居然能用，搞不懂，妈拉个鸡！！！-->
 </div>
--- zb_users/theme/qxcms/template/search.php
+++ zb_users/theme/qxcms/template/search.php
@@ -4,11 +4,20 @@ $type = $searchresult[0];
 $articles = $searchresult[1];
 {/php}
 <div class="top-content">
+    <style>
+        .Pagination{height: 55px;_height: 45px; line-height: 20px;_margin-right: 5px; color:
+            #565656;_margin-top: 20px; clear:both;margin: 0 140px}
+        .Pagination span{margin-right: 5px;display: block;float: left;margin-left: 5px;}
+        .Pagination span b{padding: 0 2px;}
+        .Pagination div {float:left}
+        .Pagination a{ font-size: 12px;text-decoration: none;display: block;float: left;color: #565656;border: 1px solid #cacaca;height: 20px;line-height: 20px;padding: 0 10px;margin: 0 2px;}
+        .Pagination .Ellipsis {line-height: 32px;overflow: hidden; display:none;}
+        cite.FormNum{display: block;float: left;line-height: 20px;height: 20px; font-style: normal; padding-left:10px;}
+        #PageNum{ color:#777;width: 30px;height: 20px;line-height: 20px; border: 1px solid #cacaca; margin:0 5px; text-align:center;}
+        .Pagination a:hover,.Pagination a.PageCur{background-color: #65B5F0;border: 1px solid #65B5F0;color: #ffffff; }
+    </style>
     {if $type==0}
     <h3>网站搜索结果:</h3>
-    {else}
-    <h3>智库中心搜索结果:</h3>
-    {/if}
     <div class="list-content">
         <ul>
             <li>
@@ -17,11 +26,33 @@ $articles = $searchresult[1];
                 {/foreach}
             </li>
         </ul>
-        {if $type==0}
         <div class="pagebar">
             {template:pagebar}
         </div>
-        {/if}
     </div>
+    {else}
+    <h3>智库中心搜索结果:</h3>
+    <div class="list-content">
+        <div class="newslist">
+            <ul>
+                <li>
+                    {foreach $articles as $article}
+                    {template:post-search}
+                    {/foreach}
+                </li>
+            </ul>
+        </div>
+    </div>
+    {/if}
 </div>
 {template:footer}
+<script type="text/javascript" src="{$host}zb_users/theme/{$theme}/js/jquery.kkPages.js"></script>
+<script>
+        $('.newslist').kkPages({
+
+            PagesClass:'.zkzx_item', //需要分页的元素
+            PagesMth:5, //每页显示个数
+            PagesNavMth:5 //显示导航个数
+        });
+
+</script>
