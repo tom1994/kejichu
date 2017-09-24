b14c9e0c2b6e8ce2825d31b6efea1d71a7a7ab42
tomxie <tomxie1994@gmail.com>
2017-09-24 11:58:52 +0800

    1.更改footer；2.更改组织架构页面，tomxie，2017-9-24 11:58:50

--- zb_users/cache/compiled/qxcms/footer.php
+++ zb_users/cache/compiled/qxcms/footer.php
@@ -1,18 +1,20 @@
+</div>
+</div>
 <!-- 页脚开始 -->
- <div class="footer">
-	<div class="foot-nav">		
-		
-	</div>
-	<div class="beian">	
-	<a href="http://www.miitbeian.gov.cn"  target="_blank"></a>
-    
-	</div>
-     <!--<div class="banquan">
-         <p></p>
-         <p>Powed by .Theme by <a href="http://www.ynjuyi.com/">千寻</a></p>
-     </div>-->
- </div>
- <!-- 页脚结束 -->
- </div>
- <?php  echo $footer;  ?></body>
+<div class="footer">
+    <div class="foot-nav">
+        
+    </div>
+    <div class="banquan">
+        <p/>
+        <p>版权所有：<a href="http://www.cupl.edu.cn/">中国政法大学</a> | 学校邮箱：<a
+                    href="mailto:cupl@cupl.edu.cn">cupl@cupl.edu.cn</a> | 京公网安备：110402430029 | 建设维护：<a
+                    href="http://xxhjsbgs.cupl.edu.cn" target="_self" textvalue="信息化建设办公室">信息化建设办公室</a>
+        </p>
+        <p>海淀校区：100088 北京市海淀区西土城路25号 | 昌平校区：102249 北京市昌平区府学路27号 | 查号电话：010－58909114</p>
+        <p/>
+    </div>
+</div>
+<!-- 页脚结束 -->
+<?php  echo $footer;  ?></body>
 </html>
